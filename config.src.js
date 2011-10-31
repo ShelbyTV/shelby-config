64ca819ccd46a33bdd2ff1b408409be115440a6a
mkrecny <mkrecny@gmail.com>
2011-10-31 14:57:04 -0400

    compiled tubes to config

--- config.src.js
+++ config.src.js
@@ -2,22 +2,39 @@ module.exports = {
   "production" : {
     "beanstalk" : {
       "uri" : '10.180.173.25',
-      "port" : "11300"
+      "port" : "11300",
+      "tubes" :{
+        "link_processing" : "link_processing",
+        "link_processing_high" : "link_processing_high",
+        "fb_add_user" : "fb_add_user",
+        "tumblr_add_user" : "tumblr_add_user",
+        "tw_backfill" : "tw_backfill",
+        "tw_stream_add" : "tw_stream_add"
+      }
     },
     "redis" : {
       "server" : "10.180.173.25",
       "port" : "6379"
-    }
+    },
   },
 
   "development" : {
     "beanstalk" : {
       "uri" : "127.0.0.1",
-      "port" : "11300"
+      "port" : "11300",
+      "tubes" :{
+        "link_processing" : "link_processing",
+        "link_processing_high" : "link_processing_high",
+        "fb_add_user" : "fb_add_user",
+        "tumblr_add_user" : "tumblr_add_user",
+        "tw_backfill" : "tw_backfill",
+        "tw_stream_add" : "tw_stream_add"
+      }
     },
     "redis" : {
       "server" : "127.0.0.1",
       "port" : "6379"
     }
+    
   }
-}
+};
